baad37d0489a0437ee6e261286ce1d4f785fbea9
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-11-12 21:15:45 -0200

    init

---
+++ poc_vue_webpack_aspnet461/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace poc_vue_webpack_aspnet461
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ poc_vue_webpack_aspnet461/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="poc_vue_webpack_aspnet461.MvcApplication" Language="C#" %>
